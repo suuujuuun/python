f81fc192c14e56442ee29b45a9fa9277c9f8a642
Cerulean.sj <163231092+suuujuuun@users.noreply.github.com>
2025-05-01 17:48:51 +0900

    Create list_slicing.py

---
+++ list_slicing.py
@@ -0,0 +1,38 @@
+userAge = [21, 22, 23, 24, 25]
+print(userAge)
+print(userAge[:4])
+print(userAge[1:4])
+print(userAge[2:4])
+print(userAge[3:4])
+print(userAge[4:4])
+
+print(userAge[0:])
+print(userAge[1:])
+print(userAge[2:])
+print(userAge[3:])
+print(userAge[4:])
+
+print(userAge[:-0])
+print(userAge[:-1])
+print(userAge[:-2])
+print(userAge[:-3])
+print(userAge[:-4])
+
+"""
+[21, 22, 23, 24, 25]
+[21, 22, 23, 24]
+[22, 23, 24]
+[23, 24]
+[24]
+[]
+[21, 22, 23, 24, 25]
+[22, 23, 24, 25]
+[23, 24, 25]
+[24, 25]
+[25]
+[]
+[21, 22, 23, 24]
+[21, 22, 23]
+[21, 22]
+[21]
+"""
