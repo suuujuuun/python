c21a291f8ff732709a53638173e4f5c2c90d08fb
Sjun <leesjun63@gmail.com>
2025-05-15 15:27:15 +0900

    Recursion

--- 01.py
+++ 01.py
@@ -1,9 +1,16 @@
-class cal:
+#put 5, answer 120{1*2*3*4*5}
+
+def recursion(fac_number):
     
+    if fac_number < 0:
+        return "음수는 계산할 수 없습니다."
+
+    if fac_number == 1:
+        return 1
+
+    return recursion(fac_number-1) * fac_number
 
 
+a = 5
+print(recursion(a))
 
-def cal():
-
-
-gdsgadsggas
